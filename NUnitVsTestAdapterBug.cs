25f4507d6e1c6e3a4cd5b9b7cb40a433ff13815d
Martin Harwig <m.harwig@ellips.com>
2022-12-05 09:40:04 +0100

    Example project using latest NUnit and NUnit3TestAdapter containing a simple unit test with a TestContext.Progress.WriteLine.

---
+++ NUnitVsTestAdapterBug.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+// ReSharper disable ObjectCreationAsStatement
+
+namespace NUnitVsTestAdapterBug
+{
+  [TestFixture]
+  public class ExampleTestFixture
+  {
+    [Test]
+    public void ExampleTest()
+    {
+      const int one = 1;
+      const int two = 2;
+      
+      TestContext.Progress.WriteLine("Example of progress message in test");
+
+      Assert.That(one + two, Is.EqualTo(3));
+    }
+  }
+}
