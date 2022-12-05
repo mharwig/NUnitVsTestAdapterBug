02db2830d86c87325041191543f09bd4688345c8
Martin Harwig <m.harwig@ellips.com>
2022-12-05 16:30:06 +0100

    also added error message

--- NUnitVsTestAdapterBug.cs
+++ NUnitVsTestAdapterBug.cs
@@ -12,6 +12,7 @@ namespace NUnitVsTestAdapterBug
       const int two = 2;
       
       TestContext.Progress.WriteLine("Example of progress message in test");
+      TestContext.Error.WriteLine("Example of error message in test");
 
       Assert.That(one + two, Is.EqualTo(3));
     }
