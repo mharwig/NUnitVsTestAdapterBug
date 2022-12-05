b30d09cfd1372f09fdc112738575ff1b9b422c0b
Martin Harwig <m.harwig@ellips.com>
2022-12-05 11:47:16 +0100

    Clean up example test

--- NUnitVsTestAdapterBug.cs
+++ NUnitVsTestAdapterBug.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Threading;
-using System.Threading.Tasks;
-using NUnit.Framework;
-using NUnit.Framework.Internal;
-
-// ReSharper disable ObjectCreationAsStatement
+﻿using NUnit.Framework;
 
 namespace NUnitVsTestAdapterBug
 {
